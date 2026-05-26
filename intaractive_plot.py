0b12d05250d4ae37b53b3e4b57c7972174d814fd
MatzeGit3 <wm4310@mci4me.at>
2026-05-26 16:15:34 +0200

    hr_plot

---
+++ intaractive_plot.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from read_pandas import read_my_csv
+from read_pandas import make_plot
+from read_pandas import read_my_activity, make_power_hr_plot
+
+# Wo startet sie Zeitreihe
+# Wo endet sich
+# Was ist die Maximale und Minimale Spannung
+# Grafik
+tab1, tab2 = st.tabs(["EKG-Data", "Power-Data"])
+
+with tab1:
+    st.header("EKG-Data")
+    st.write("# My Plot")
+
+    df = read_my_csv()
+    fig = make_plot(df)
+
+    st.plotly_chart(fig)
+
+with tab2:
+    st.header("Power-Data")
+    st.write("# HR")
+
+    df = read_my_activity()
+    fig = make_power_hr_plot(df)
+    st.plotly_chart(fig)
---
+++ read_pandas.py
@@ -0,0 +1,61 @@
+import pandas as pd 
+import plotly.express as px
+
+def read_my_csv():
+    # Einlesen eines Dataframes
+    ## "\t" steht für das Trennzeichen in der txt-Datei (Tabulator anstelle von Beistrich)
+    ## header = None: es gibt keine Überschriften in der txt-Datei
+    df = pd.read_csv("data/ekg_data/01_Ruhe.txt", sep="\t", header=None)
+
+    # Setzt die Columnnames im Dataframe
+    df.columns = ["Messwerte in mV","Zeit in ms"]
+    
+    # Gibt den geladen Dataframe zurück
+    return df
+
+def read_my_activity():
+    df = pd.read_csv("data/activities/activity.csv")
+    df["time_seconds"] = df["Duration"].cumsum()
+    return df
+   # df["time_seconds"]
+
+
+
+def make_hr_plot(df):
+    fig = px.line(
+        df,
+        x="time_seconds",
+        y="HeartRate",
+        title="Herzfrequenz über Zeit",
+        labels={
+            "time_seconds": "Zeit [s]",
+            "HeartRate": "Herzfrequenz [bpm]"
+        }
+    )
+    return fig
+     
+    
+def add_hr_zones(df, hr_max):
+    bins = [0, hr_max * 0.6, hr_max * 0.7, hr_max * 0.8, hr_max * 0.9, hr_max]
+    labels = ["Zone 1", "Zone 2", "Zone 3", "Zone 4", "Zone 5"]
+
+    df["HR Zone"] = pd.cut(
+        df["HeartRate"],
+        bins=bins,
+        labels=labels,
+        include_lowest=True
+    )
+
+    return df
+
+
+def make_plot(df):
+
+    # Erstellte einen Line Plot, der ersten 2000 Werte mit der Zeit aus der x-Achse
+    fig = px.line(df.head(2000), x= "Zeit in ms", y="Messwerte in mV")
+    return fig
+
+if __name__=="__main__":
+    activity_df = read_my_activity()
+
+    print(activity_df)
